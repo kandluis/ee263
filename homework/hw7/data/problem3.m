6f9f3975296c060174b2a33e21bbf56a683053b5
Luis Perez <luis.perez.live@gmail.com>
2019-08-13 20:21:44 -0700

    Making progress on homework

--- homework/hw7/data/problem3.m
+++ homework/hw7/data/problem3.m
@@ -3,8 +3,12 @@ bs_det_data;
 
 % Find the eigenvalues of Y*Y.T
 [U,S,V] = svd(Y);
+
+% Take the eigenvector with largest eigenvalue.
 eigenValue = S(1,1)^2;
 u = U(:,1);
+
+% Normalize it to be the right size.
 w = u / norm(u) * T / eigenValue; 
 
 % Verify w satisfies optimality conditions with some tolerance.
@@ -13,4 +17,13 @@ assert(all((w  - 1/eigenValue * Y * Y' * w) < eps));
 assert(all((w' * Y * Y' * w - T) < eps));
 
 % Plot histogram
+figure
 hist(w'*Y, 50);
+title('Histogram of w^TY (estimated signal) values');
+xlabel('Estimated signal value')
+ylabel('Count');
+saveas(gcf, 'blind_signal.jpg');
+
+% Compute the estimated \hat{s} (or it's negation).
+shat = sign(w'*Y);
+errorRate = sum(shat ~= s) / T;
